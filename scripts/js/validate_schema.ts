8cfbe0c1168bfb800a6394200c01d6701097ce4b
Marek Rusinowski <marekrusinowski@gmail.com>
2023-03-31 23:09:30 +0200

    Make validate_schema output stable

--- scripts/js/validate_schema.ts
+++ scripts/js/validate_schema.ts
@@ -1,19 +1,20 @@
 import Ajv from "npm:ajv@^8.12.0/dist/2020.js";
+import stringify from "npm:json-stable-stringify@^1.0.2";
 
 const schema = await Deno.readTextFile(Deno.args[0]);
 const json_data = await Deno.readTextFile(Deno.args[1]);
 
 const ajv = new Ajv({
-	allErrors: true,
-	useDefaults: true
+    allErrors: true,
+    useDefaults: true
 });
 const validate = ajv.compile(JSON.parse(schema));
 const data = JSON.parse(json_data);
 if (!validate(data)) {
-	for (let error of validate.errors) {
-		console.error(error);
-	}
-	throw new Error("Validation of data agains schema failed");
+    for (const error of validate.errors) {
+        console.error(error);
+    }
+    throw new Error("Validation of data agains schema failed");
 }
 
-await Deno.writeTextFile(Deno.args[2], JSON.stringify(data, null, 4));
+await Deno.writeTextFile(Deno.args[2], stringify(data, {space: '    '}) + '\n');
