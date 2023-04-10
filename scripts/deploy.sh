5705ecbcdfbabcc4389298897b8934ac01e75259
Marek Rusinowski <marekrusinowski@gmail.com>
2023-04-10 20:13:46 +0200

    Add deployment script to R2 bucket

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
 
+set -euo pipefail
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <commit> <set-latest>"
+    exit 1
+fi
+
+if [[ ! -d gen ]]; then
+    echo "gen directory doesn't exist"
+    exit 1
+fi
+
 COMMIT="$1"
 SET_LATEST="$2"
 
 echo "Deploying commit: $COMMIT, set latest: $SET_LATEST"
+
+for var in CLOUDFLARE_ACCESS_KEY_ID CLOUDFLARE_SECRET_ACCESS_KEY CLOUDFLARE_ACCOUNT_ID R2_BUCKET_NAME; do
+    if [[ -z "${!var}" ]]; then
+        echo "$var is not set"
+        exit 1
+    fi
+done
+
+TMP_FILE="$(mktemp /tmp/rclone-XXXXXX.conf)"
+
+cat << EOF > $TMP_FILE
+[r2]
+type = s3
+provider = Cloudflare
+access_key_id = $CLOUDFLARE_ACCESS_KEY_ID
+secret_access_key = $CLOUDFLARE_SECRET_ACCESS_KEY
+endpoint = https://$CLOUDFLARE_ACCOUNT_ID.r2.cloudflarestorage.com
+acl = private
+chunk_size = 20M
+upload_concurrency = 2
+EOF
+
+trap "rm -f $TMP_FILE" EXIT
+
+rclone --config $TMP_FILE copy gen "r2:$R2_BUCKET_NAME/$COMMIT"
+
+if [ "$SET_LATEST" = "true" ]; then
+    printf "$COMMIT" | rclone --config $TMP_FILE --header-upload "Content-Type: text/plain" rcat "r2:$R2_BUCKET_NAME/HEAD"
+fi
+
+echo "Done"
