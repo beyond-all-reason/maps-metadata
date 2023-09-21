b9249c1a75bba01ff5fe3c8f3ef45c03a391e856
RebelNode <kipale@kipale.com>
2023-09-21 14:52:08 +0300

    Various changes requested by Marek.

--- scripts/py/gen_nextmap_maplists.py
+++ scripts/py/gen_nextmap_maplists.py
@@ -18,7 +18,7 @@ def get_data(input_file):
     certified_maps = []
     uncertified_maps = []
     maps_1v1 = []
-  
+
     for i in teamsizes:
         teamsize_dict[i] = []
     for i in ffasizes:
@@ -26,110 +26,84 @@ def get_data(input_file):
     for i in teamffasizes:
         teamffasize_dict[i] = []
     
-    for i in contents:
-        if not contents[i]["inPool"]:
+    for map in contents.values():
+        if not map["inPool"]:
             continue
-        isTeam = False
-        isFfa = False
-        is1v1 = False
+        is_team = False
+        is_ffa = False
+        is_1v1 = False
         mapname = ''
-        playerCount = 0
+        player_count = 0
 
-        if "springName" in contents[i].keys():
-            mapname = contents[i]["springName"]
-        if "gameType" in contents[i].keys() and "team" in contents[i]["gameType"]:
-            isTeam = True
-        if "gameType" in contents[i].keys() and "ffa" in contents[i]["gameType"]:
-            isFfa = True
-        if "gameType" in contents[i].keys() and "1v1" in contents[i]["gameType"]:
-            is1v1 = True
-        if "playerCount" in contents[i].keys():
-            playerCount = contents[i]["playerCount"]
+
+        mapname = map["springName"]
+        if "gameType" in map and "team" in map["gameType"]:
+            is_team = True
+        if "gameType" in map and "ffa" in map["gameType"]:
+            is_ffa = True
+        if "gameType" in map and "1v1" in map["gameType"]:
+            is_1v1 = True
+        if "playerCount" in map:
+            player_count = map["playerCount"]
         #32 player ffa or other such sillyness not supported in !nextmap
-        if playerCount > 16:
-            playerCount = 16
+        if player_count > 16:
+            player_count = 16
+        if player_count < 2:
+            player_count = 2
 
-        if "startboxesSet" in contents[i].keys():
-            for j in contents[i]["startboxesSet"]:
-                if "maxPlayersPerStartbox" in contents[i]["startboxesSet"][j].keys():
+        if "startboxesSet" in map:
+            for startboxes_info in map["startboxesSet"]:
+                if "maxPlayersPerStartbox" in map["startboxesSet"][startboxes_info]:
                     numberOfTeams = 0
-                    for k in contents[i]["startboxesSet"][j]["startboxes"]:
+                    for k in map["startboxesSet"][startboxes_info]["startboxes"]:
                         numberOfTeams = numberOfTeams + 1
-                    maxPlayersPerStartbox = contents[i]["startboxesSet"][j]["maxPlayersPerStartbox"]
+                    maxPlayersPerStartbox = map["startboxesSet"][startboxes_info]["maxPlayersPerStartbox"]
 
                     # add maps to teamsize_dict
-                    if numberOfTeams == 2:
-                        x = 0
-                        for l in teamsize_dict:
-                            if x + 2 <= maxPlayersPerStartbox and x + 2 >= math.floor(maxPlayersPerStartbox/2):
-                                teamsize_dict[l].append(mapname)
-                            x = x + 1
+                    if numberOfTeams == 2 and numberOfTeams*maxPlayersPerStartbox <= 16:
+                        for x in range(2,maxPlayersPerStartbox+1): 
+                            if x >= math.floor(maxPlayersPerStartbox/2):
+                                teamsize_dict['v'.join([str(x)] * numberOfTeams)].append(mapname)
                     
                     # add maps to teamffasize_dict:
-                    if numberOfTeams == 3:
-                        if maxPlayersPerStartbox >= 2:
-                            teamffasize_dict['2v2v2'].append(mapname)
-                        if maxPlayersPerStartbox >= 3:
-                            teamffasize_dict['3v3v3'].append(mapname)
-                        if maxPlayersPerStartbox >=4:
-                            teamffasize_dict['4v4v4'].append(mapname)
-                        if maxPlayersPerStartbox >=5:
-                            teamffasize_dict['5v5v5'].append(mapname)
-                    if numberOfTeams == 4:
-                        if maxPlayersPerStartbox >= 2:
-                            teamffasize_dict['2v2v2v2'].append(mapname)
-                        if maxPlayersPerStartbox >= 3:
-                            teamffasize_dict['3v3v3v3'].append(mapname)
-                        if maxPlayersPerStartbox >=4:
-                            teamffasize_dict['4v4v4v4'].append(mapname)
-                    if numberOfTeams == 5:
-                        if maxPlayersPerStartbox >= 2:
-                            teamffasize_dict['2v2v2v2v2'].append(mapname)
-                        if maxPlayersPerStartbox >= 3:
-                            teamffasize_dict['3v3v3v3v3'].append(mapname)
-                    if numberOfTeams == 6:
-                        if maxPlayersPerStartbox >= 2:
-                            teamffasize_dict['2v2v2v2v2v2'].append(mapname)
-                    if numberOfTeams == 7:
-                        if maxPlayersPerStartbox >= 2:
-                            teamffasize_dict['2v2v2v2v2v2v2'].append(mapname)
-                    if numberOfTeams == 8:
-                        if maxPlayersPerStartbox >= 2:
-                            teamffasize_dict['2v2v2v2v2v2v2v2'].append(mapname)
+                    if numberOfTeams != 2 and numberOfTeams*maxPlayersPerStartbox <= 16:
+                        for x in range(2,maxPlayersPerStartbox+1):
+                            if x >= math.floor(maxPlayersPerStartbox/2):
+                                teamffasize_dict['v'.join([str(x)] * numberOfTeams)].append(mapname)
 
                 # if a map didn't have "maxPlayersPerStartbox" set for its startboxes, but it's a teamgame map with startboxes for 2 teams, we'll use playerCount instead:
-                if not "maxPlayersPerStartbox" in contents[i]["startboxesSet"][j].keys() and playerCount and isTeam:
+                if not "maxPlayersPerStartbox" in map["startboxesSet"][startboxes_info] and player_count and is_team:
                     numberOfTeams = 0
-                    for k in contents[i]["startboxesSet"][j]["startboxes"]:
+                    for k in map["startboxesSet"][startboxes_info]["startboxes"]:
                         numberOfTeams = numberOfTeams + 1
-                    if numberOfTeams == 2:
-                        x = 0
-                        for l in teamsize_dict:
-                            if x + 2 <= math.floor(playerCount/2) and x + 2 >= math.floor(playerCount/4):
-                                teamsize_dict[l].append(mapname)
-                            x = x + 1
-        
+                    if numberOfTeams == 2 and player_count >=4:
+                        for x in range(2,math.floor(player_count/2)+1): 
+                            if x >= math.floor(player_count/4):
+                                teamsize_dict['v'.join([str(x)] * numberOfTeams)].append(mapname)
+
         # add maps to ffasize_dict
-        if isFfa:
-            x = 0
-            for j in ffasize_dict:
-                if x + 3 <= playerCount and x + 3 >= math.floor(playerCount/2):
-                    ffasize_dict[j].append(mapname)
-                x = x + 1
-                    
+        if is_ffa:
+            for x in range(3,17):
+                if x <= player_count and x >= math.floor(player_count/2):
+                    ffasize_dict['ffa' + str(x)].append(mapname)
+
         # add maps to certified and uncertified lists
-        if contents[i]["certified"] == True:
+        if map["certified"] == True:
             certified_maps.append(mapname)
         else:
             uncertified_maps.append(mapname)
         
-        #add maps to 1v1 list
-        if is1v1:
+        # add maps to 1v1 list
+        if is_1v1:
             maps_1v1.append(mapname)
 
     combined_dict = {**teamsize_dict,**ffasize_dict,**teamffasize_dict}
 
-    #if combined_dict has empty lists then add ".*" (meaning all maps) to that list
+    # make the lists more human-readable
+    for maplist in combined_dict.values():
+        maplist.sort()
+
+    # if combined_dict has empty lists then add ".*" (meaning all maps) to that list
     for i in combined_dict:
         if len(combined_dict[i]) == 0:
             combined_dict[i].append('.*')
@@ -147,23 +121,22 @@ def get_output_string(combined_dict, certified_maps, uncertified_maps, maps_1v1)
 
 [certified]
 """
-    for i in certified_maps:
-        output_string = output_string + i + '\n'
+    output_string += '\n'.join(certified_maps)
+    output_string = output_string + '\n'
 
     output_string = output_string + '\n[uncertified]\n'
-    for i in uncertified_maps:
-        output_string = output_string + i + '\n'
+    output_string += '\n'.join(uncertified_maps)
+    output_string = output_string + '\n'
+
     output_string = output_string + '\n[small]\n.*\n\n[medium]\n.*\n\n[large]\n.*\n\n[extraLarge]\n.*\n\n[misc]\n.*\n\n[1v1]\n'
 
-    for i in maps_1v1:
-        output_string = output_string + i + '\n'
-    output_string = output_string + '\n'
+    output_string += '\n'.join(maps_1v1)
+    output_string = output_string + '\n\n'
     
     for i in combined_dict:
         output_string = output_string + '[' + i + ']\n'
-        for j in combined_dict[i]:
-            output_string = output_string + j + '\n'
-        output_string = output_string + '\n'
+        output_string += '\n'.join(combined_dict[i])
+        output_string = output_string + '\n\n'
     
     return output_string
 
