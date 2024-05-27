dd6597ec7abb33be94f3b7aa35af03da4af416e3
Marek Rusinowski <marekrusinowski@gmail.com>
2024-05-28 00:18:50 +0200

    Manage rotationType instead of mapList in spads (#332)
    See https://github.com/beyond-all-reason/spads_config_bar/pull/129

--- scripts/py/update_spads_conf.py
+++ scripts/py/update_spads_conf.py
@@ -9,10 +9,12 @@ def update_spads_conf(spads_conf_path, custom_map_lists):
     with open(spads_conf_path, "r", encoding="utf-8") as f:
         conf = f.read()
 
-    comment = '# [automanaged] mapList value is managed by maps-metadata automation\n'
-    l = "|".join(["all"] + custom_map_lists)
+    comment = "# [automanaged] value is managed by maps-metadata automation\n"
+    options = [f"map;{m}" for m in ["certified"] + custom_map_lists]
     new_conf = re.sub(
-        r"(?m)^# \[automanaged\].*\n^mapList:.*$", comment + "mapList:" + l, conf
+        r"(?m)^# \[automanaged\].*\n^rotationType:.*$",
+        f'{comment}rotationType:{"|".join(options)}',
+        conf,
     )
 
     with open(spads_conf_path, "w", encoding="utf-8") as f:
